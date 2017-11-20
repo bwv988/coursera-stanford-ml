6799e8d718e4a6b8b2b35b8ddca9a55a7faf289d
Ralph Schlosser <ralph.schlosser@gmail.com>
2017-11-20 17:43:25 +0000

    More progress.

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -12,6 +12,7 @@ m = length(y); % number of training examples
 J = 0;
 grad = zeros(size(theta));
 
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost and gradient of regularized linear 
 %               regression for a particular choice of theta.
@@ -19,16 +20,20 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+% Define some variables for later use.
+v = (X * theta - y);
+u = v .^ 2;
 
+% We are not regularizing theta0, so setting it to 0.
+theta(1) = 0;
 
+% Cost function, vectorized, avoding loops. ;)
+J = 1 / (2 * m) * (sum(u) + lambda * sum(theta .^ 2));
 
+% Now calc. the gradient. The below will take care of the summation as well.
+w = X' * v;
 
-
-
-
-
-
-
+grad = (1 / m) * (w + lambda * theta);
 
 % =========================================================================
 
